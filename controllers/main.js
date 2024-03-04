cc22335f97885dea2ea08cf8d65cd7e1404ef641
minhquang3225 <60904218+minhquang3225@users.noreply.github.com>
2024-03-04 21:03:29 +0700

    Comit dot 2

--- controllers/main.js
+++ controllers/main.js
@@ -19,62 +19,101 @@ function renderNavPills(arrPill) {
     `
     arrPill.map((pill, index) => {
         if (index == 0) {
-            pillContent+= `
+            pillContent += `
         <li class="nav-item">
             <a class="nav-link active" data-toggle="tab" data-target="#${pill.type}">${pill.showName}</a>
             
         </li>
     `;
-            tabContent +=  `
+            tabContent += `
             <div class="tab-pane fade show active" id="${pill.type}" role="tabpanel" aria-labelledby="home-tab">${listUI}</div>
             
             `
-            
+
         }
         else {
-            pillContent+= `
+            pillContent += `
         <li class="nav-item">
             <a class="nav-link " data-toggle="tab" data-target="#${pill.type}" >${pill.showName}</a>
         </li>
     `;
-    tabContent +=  `
+            tabContent += `
             <div class="tab-pane fade show" id="${pill.type}" role="tabpanel">${listUI}</div>
             ${listUI}
             `
 
         }
     });
-   
+
     navPill.innerHTML = pillContent;
     navTab.innerHTML = tabContent;
 }
-function renderProduct(arrProducts){
-    
-    arrProducts.forEach(item=>{
-        let ele = document.querySelector(`#${item.type} .row`);
+function renderProduct(arrProducts) {
 
-        let content=`
+    arrProducts.forEach(item => {
+        let ele = document.querySelector(`#${item.type} .row`);
+        let content = `
             <div class="col-4 my-4 text-center" id="${item.id}">
                 <img src="${item.imgSrc_jpg}"/>
                 <h5>${item.name}</h5>
-                <button class="btn btn-primary dressing" data-image='${item.imgSrc_png} data-type='${item.type}'>Thử đồ</button>
+                <button class="btn btn-primary dressing" data-image='${item.imgSrc_png}' data-type="${item.type}">Thử đồ</button>
             </div>
         `
-        ele.innerHTML+= content;
+        ele.innerHTML += content;
 
     })
 }
-function table(){}
+function table() { }
 async function renderUI() {
     let data = await getData();
     renderNavPills(data.navPills);
     renderProduct(data.tabPanes);
+    btnEvent();
 
 }
-function dressing(){
-    let dressingBtn = documnent.querySelectorAll('.dressing');
-    dressingBtn.addEventListener('click', (e)=>{
-        
+function btnEvent() {
+    let dressingBtn = document.querySelectorAll('.dressing');
+    dressingBtn.forEach(btn=>{
+        btn.addEventListener('click', (e) => {
+            let partID;
+            let img = e.target.dataset.image;
+            switch (e.target.dataset.type) {
+                case 'topclothes':
+                    partID = "bikinitop"
+                    break;
+                case 'botclothes':
+                    partID = ""
+                    break;
+                case 'shoes':
+                    partID = "body"
+                    break;
+                case 'handbags':
+                    partID = "body"
+                    break;
+                case 'necklaces':
+                    partID = "body"
+                    break;
+                case 'hairstyle':
+                    partID = "body"
+                    break;
+                case 'background':
+                    partID = "background"
+                    break;
+    
+                default:
+                    break;
+            }
+            dressing(partID, img);
+        })
     })
+}
+function dressing(partID, img){
+    let element = document.querySelector(`.contain .${partID}`);
+    if (partID == 'bikinitop') {
+  
+    }
+    element.style.backgroundImage =`url(${img})`;
+    element.style.backgroundPosition =  `top`
+    
 }
 renderUI();
